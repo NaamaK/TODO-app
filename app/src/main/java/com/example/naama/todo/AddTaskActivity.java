99c001b00fff087c551b61d00e8547b7b2cbc364
NaamaK <naama.kapach@gmail.com>
2015-10-31 14:11:05 +0200

    app adding new items to the listView

--- app/src/main/java/com/example/naama/todo/AddTaskActivity.java
+++ app/src/main/java/com/example/naama/todo/AddTaskActivity.java
@@ -1,13 +1,28 @@
 package com.example.naama.todo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        intent= getIntent();
+    }
+
+    public void saveAndReturn(View view) {
+        EditText item= (EditText) findViewById(R.id.addItemText);
+        String newItem= item.getText().toString();
+        intent.putExtra("KEY", newItem);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
