f84571e081286e35f09f29099b07298f735b4275
NaamaK <naama.kapach@gmail.com>
2015-11-01 12:51:51 +0200

    Problem fixed

--- app/src/main/java/com/example/naama/todo/TaskListActivity.java
+++ app/src/main/java/com/example/naama/todo/TaskListActivity.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -21,7 +22,7 @@ import java.util.Vector;
 
 public class TaskListActivity extends AppCompatActivity {
 
-    private final ArrayList<String> itemArray= new ArrayList();
+    private final ArrayList<String> itemArray = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,67 +33,28 @@ public class TaskListActivity extends AppCompatActivity {
         itemArray.add("2");
         itemArray.add("3");
         itemArray.add("4");
+        updateListView(itemArray);
 
-        ListView list= (ListView)findViewById(R.id.myListView);
-        list.setAdapter(new MyListAdapter(this, R.layout.xml_item, itemArray));
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == 1) {
-            String itemName= data.getStringExtra("KEY");
-            if (itemName==null) itemName="NULL";
-            populateListView(itemName);
+            String itemName = data.getStringExtra("KEY");
+            if (itemName == null) itemName = "NULL";
+            itemArray.add(itemName);
+            updateListView(itemArray);
         }
     }
 
-    private void populateListView(String str) {
-        itemArray.add(str);
+    private void updateListView(ArrayList<String> itemArray) {
+        ListView list = (ListView) findViewById(R.id.myListView);
+        list.setAdapter(new MyListAdapter(itemArray));
     }
 
     public void addTask(View view) {
-        Intent intent= new Intent(TaskListActivity.this, AddTaskActivity.class);     //activate the new activity
-        startActivityForResult(intent,1);                                            //initiate the activity
-    }
-
-    private class MyListAdapter extends ArrayAdapter<String> {
-
-        private int layout;
-
-        public MyListAdapter(Context context, int resource, List<String> objects) {
-            super(context, resource, objects);
-            layout= resource;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            ViewHolder mainHolder= null;
-            if (convertView == null) {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView= inflater.inflate(layout, parent, false);
-                ViewHolder viewHolder= new ViewHolder();
-                viewHolder.item= (TextView) convertView.findViewById(R.id.itemText);
-                viewHolder.btn= (Button) convertView.findViewById(R.id.doneBtn);
-                viewHolder.btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(getContext(), itemArray.get(position), Toast.LENGTH_SHORT).show();
-                    }
-                });
-                convertView.setTag(viewHolder);
-            }
-            else {
-                mainHolder= (ViewHolder) convertView.getTag();
-                mainHolder.item.setText(getItem(position));
-            }
-
-            return convertView;
-        }
-
-        public class ViewHolder {
-            TextView item;
-            Button btn;
-        }
+        Intent intent = new Intent(TaskListActivity.this, AddTaskActivity.class);     //activate the new activity
+        startActivityForResult(intent, 1);                                            //initiate the activity
     }
 }
